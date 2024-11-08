34c7a4c313f9f57390979ba032e337aa8033576b
KluEvo <30702547+KluEvo@users.noreply.github.com>
2024-11-08 16:49:37 -0600

    updated ingredient getting to be more versatile

--- datafetch.py
+++ datafetch.py
@@ -1,47 +1,60 @@
 import requests
 from bs4 import BeautifulSoup
+import re
+from directions import get_directions, fetch_recipe_page 
 
-def fetch_recipe_page(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.text
-    else:
-        raise Exception(f"Failed to fetch the page. Status code: {response.status_code}")
-def parse_recipe(html_content):
+def get_ingredients(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
     # print(soup.title.string)
     # print(len(soup.find_all('p')))
     ingredients = []
     directions = []
-    for text in soup.find_all('p'):
-        if not (text.string):
-            ing = {}
-            # print(text)
-            # print(text.span.string)
-            for spanner in text.find_all('span'):
-                if spanner.has_attr("data-ingredient-quantity"):
-                    ing["amount"] = spanner.text
-                elif spanner.has_attr("data-ingredient-unit"):
-                    ing["unit"] = spanner.text
-                elif spanner.has_attr("data-ingredient-name"):
-                    ing["name"] = spanner.text
-            ingredients.append(ing)
-        else:
-            directions
+    # Define a function to check if 'options' is in the class attribute
+    def has_options_class(tag):
+        return tag.name == 'ul' and 'ingredients' in tag.get('class', [])
+
+    # Use the find method with the custom function
+    ingListText = soup.find(name='ul', class_=re.compile('ingredients'))
+    # print(ingListText)
 
 
+    # ingListText = soup.find_all('ul')[0]
+    # # ulists = 
+    # for ulist in soup.find_all('ul'):
+    #     if not ulist.has_attr('class'):
+    #         continue
+    #     elif 'ingredients' in ulist['class'][0]:
+    #         ingListText = ulist
+    #         break
+    # print(ingListText)
+    for text in ingListText.find_all('p'):
+        ing = {}
+        # print(text)
+        # print(text.span.string)
+        for spanner in text.find_all('span'):
+            if spanner.has_attr("data-ingredient-quantity"):
+                ing["amount"] = spanner.text
+            elif spanner.has_attr("data-ingredient-unit"):
+                ing["unit"] = spanner.text
+            elif spanner.has_attr("data-ingredient-name"):
+                ing["name"] = spanner.text
+        ingredients.append(ing)
+
+    return ingredients
+
+def parse_recipe(html_content):
     tools = []
     methods = []
-
     return {
-        'ingredients': ingredients,
-        'directions': directions,
+        'ingredients': get_ingredients(html_content),
+        'directions': get_directions(html_content),
         'tools': tools,
         'methods': methods
     }
 
 # Example usage
 url = "https://www.allrecipes.com/recipe/7011/chinese-steamed-buns/"
+# url = "https://www.seriouseats.com/baked-french-toast-casserole-recipe-8740716"
 html_content = fetch_recipe_page(url)
 
 # Example usage
