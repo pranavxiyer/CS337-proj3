105d6ddbbfdad948d777104e0f31f63d2a73a9c7
pranavxiyer <pranavxiyer@gmail.com>
2024-11-13 17:19:21 -0600

    split directions

--- directions.py
+++ directions.py
@@ -5,10 +5,13 @@ import nltk
 from nltk.corpus import verbnet
 from nltk.corpus import wordnet as wn
 from nltk.stem.wordnet import WordNetLemmatizer
+from rapidfuzz import fuzz
 
 spacy_model = spacy.load("en_core_web_lg")
 cooking_class = verbnet.vnclass('cooking-45.3')
 method_verbs = [member.get("name") for member in cooking_class.findall("MEMBERS/MEMBER")]
+# print(method_verbs)
+# print(fuzz.ratio("saute", "sautee"))
 
 # print(method_verbs)
 
@@ -22,8 +25,25 @@ def fetch_recipe_page(url):
     else:
         raise Exception(f"Failed to fetch the page. Status code: {response.status_code}")
 
+# def get_directions_old(html_content):
+#     directions = {}
+#     soup = BeautifulSoup(html_content, 'html.parser')
+#     direction_results = soup.find(id="mm-recipes-steps_1-0")
+#     direction_ol = direction_results.find("ol")
+#     direction_li = direction_ol.find_all("li")
+    
+#     for i in range(len(direction_li)):
+#         p = direction_li[i].find("p")
+#         real_step = i + 1
+#         step_text = "Step " + str(real_step)
+#         step_direction = p.text.strip()
+#         directions[step_text] = step_direction
+    
+#     return directions
+
 def get_directions(html_content):
     directions = {}
+    directions_list = []
     soup = BeautifulSoup(html_content, 'html.parser')
     direction_results = soup.find(id="mm-recipes-steps_1-0")
     direction_ol = direction_results.find("ol")
@@ -31,11 +51,19 @@ def get_directions(html_content):
     
     for i in range(len(direction_li)):
         p = direction_li[i].find("p")
+        step_direction = p.text.strip()
+        step_split = step_direction.split(".")
+
+        for j in range(len(step_split)):
+            if len(step_split[j]) > 1:
+                real_step_direction = step_split[j].lstrip() + "."
+                directions_list.append(real_step_direction)
+    
+    for i in range(len(directions_list)):
         real_step = i + 1
         step_text = "Step " + str(real_step)
-        step_direction = p.text.strip()
-        directions[step_text] = step_direction
-    
+        directions[step_text] = directions_list[i]
+
     return directions
 
 def get_cooks_note():
@@ -87,10 +115,17 @@ def get_methods_spacy(directions_dictionary):
 
 
 # recipe_url = "https://www.allrecipes.com/recipe/8462067/spinach-artichoke-garlic-naan-pizza"
-# #recipe_url = "https://www.allrecipes.com/chicken-carbonara-pasta-bake-recipe-7969899"
+# recipe_url = "https://www.allrecipes.com/chicken-carbonara-pasta-bake-recipe-7969899"
+# recipe_url = "https://www.seriouseats.com/hong-kong-style-french-toast-recipe-8605272"
 # recipe_html = fetch_recipe_page(recipe_url)
 # directions = get_directions(recipe_html)
-# print("NLTK")
+# print(f"directions: {directions}")
 # get_methods_nltk(directions)
-# print("SPACY")
 # get_methods_spacy(directions)
+
+## TESTING FOR DIFF WEBSITES
+# recipe_url = "https://www.allrecipes.com/recipe/8462067/spinach-artichoke-garlic-naan-pizza"
+# recipe_url = "https://www.allrecipes.com/chicken-carbonara-pasta-bake-recipe-7969899"
+# recipe_url = "https://www.seriouseats.com/hong-kong-style-french-toast-recipe-8605272"
+# recipe_html = fetch_recipe_page(recipe_url)
+# get_directions2(recipe_html)
