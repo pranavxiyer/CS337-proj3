b1668cb0ac69484c0a4e0e1f01e9a88bce52c663
KluEvo <keene.ruiyang.lu@gmail.com>
2024-11-29 16:38:21 -0600

    healthyv2 worksish

--- healthyv2.py
+++ healthyv2.py
@@ -7,15 +7,13 @@ import nltk
 
 nlp = spacy.load('en_core_web_sm')
 
-def get_healthier_alternative(ingredient):
-    
-    ingr = ingredient['name'].lower()
 
-    unhealthy_to_healthy = {
+
+def replacementIngredients():
+    return {
         'sugar': 'honey',
         'butter': 'olive oil',
-        'white flour': 'whole wheat flour',
-        'salt': 'herbs',
+        'flour': 'whole wheat flour',
         'cream': 'low-fat yogurt',
         'mayonnaise': 'greek yogurt',
         'bacon': 'turkey bacon',
@@ -25,16 +23,24 @@ def get_healthier_alternative(ingredient):
         'cheese': 'low-fat cheese',
         'oil': 'olive oil'
     }
-    for unhealthy, healthy in unhealthy_to_healthy.items():
-        if unhealthy in ingr:
+
+def get_healthier_alternative(ingredient):
+    
+    ingr = ingredient['name'].lower()
+
+    for unhealthy, healthy in replacementIngredients().items():
+        if unhealthy in ingr and healthy not in ingr:
             ingredient['name'] = healthy
-            return healthy
+            ingredient['descriptor'] = ''
+            return ingredient
 
         synsets = wordnet.synsets(unhealthy)
         for synset in synsets:
             for lemma in synset.lemmas():
                 if lemma.name() in ingr:
-                    return healthy
+                    ingredient['name'] = healthy
+                    ingredient['descriptor'] = ''
+                    return ingredient
 
     return ingredient
 
@@ -69,11 +75,17 @@ def make_recipe_healthier(recipe):
 
     recipe['methods'] = [primary_methods, secondary_methods]
 
-    for step in recipe['directions']:
+    for iter, step in recipe['directions'].items():
         doc = nlp(step)
         for token in doc:
             if token.lemma_ in healthier_methods:
                 step = step.replace(token.text, healthier_methods[token.lemma_])
+            replIngs = replacementIngredients()
+            if token.lemma_ in replIngs:
+                
+                step = step.replace(token.text, replIngs[token.lemma_])
+        # print(step)
+
 
     return recipe
 
@@ -81,30 +93,6 @@ def make_recipe_healthier(recipe):
 
 if __name__ == "__main__":
     # Example usage
-    recipe = {
-        'ingredients': [
-            {'amount': '1 3/4', 'unit': 'cups', 'name': 'all-purpose flour, divided', 'descriptor': 'all-purpose', 'preparation': 'divided'}, 
-            {'amount': '3/4', 'unit': 'cup', 'name': 'warm water, divided', 'descriptor': 'warm', 'preparation': 'divided'}, 
-            {'amount': '1', 'unit': 'tablespoon', 'name': 'active dry yeast', 'descriptor': 'dry', 'preparation': None}, 
-            {'amount': '1', 'unit': 'teaspoon', 'name': 'white sugar', 'descriptor': 'white', 'preparation': None}, 
-            {'amount': '2', 'unit': 'tablespoons', 'name': 'white sugar', 'descriptor': 'white', 'preparation': None}, 
-            {'amount': '1', 'unit': 'tablespoon', 'name': 'vegetable oil', 'descriptor': 'vegetable', 'preparation': None}, 
-            {'amount': '1/4', 'unit': 'teaspoon', 'name': 'salt', 'descriptor': None, 'preparation': None}, 
-            {'amount': '1/2', 'unit': 'teaspoon', 'name': 'baking powder', 'descriptor': None, 'preparation': None}
-        ],
-        'directions': {
-            'Step 1': 'Gather all ingredients.', 
-            'Step 2': 'Mix together 1/4 cup flour, 1/4 cup warm water, yeast, and 1 teaspoon sugar in a large bowl.', 
-            'Step 3': 'Allow to sit for 30 minutes.', 
-            'Step 4': 'Mix in remaining 1 1/2 cups flour, remaining 1/2 cup warm water, 2 tablespoons sugar, vegetable oil, and salt.', 
-            'Step 5': 'Knead until dough is smooth and elastic.', 
-            'Step 6': 'Transfer to a greased bowl, roll to coat with oil, and let sit until tripled in size, 2 1/2 to 3 hours.', 
-            'Step 7': 'Punch down dough and spread out on a floured board.', 'Step 8': 'Sprinkle baking powder evenly on surface of dough; knead for 5 minutes.', 'Step 9': 'Divide dough in half; set aside one half in a covered bowl.', 'Step 10': 'Divide remaining half into 12 equal pieces.', 'Step 11': 'Shape each into a ball; transfer each ball to a small square of waxed paper with the smooth surface facing up.', 'Step 12': 'Repeat portioning and shaping with remaining dough half.', 'Step 13': 'Cover all 24 dough balls and let sit until doubled in size, about 30 minutes.', 'Step 14': 'Bring some water to a boil in a wok, then reduce heat to medium and keep water at a low boil.', 'Step 15': 'Place the steam plate on a small wire rack in the middle of the wok, leaving at least 2 inches of space between the plate and the wok.', 'Step 16': 'Working in batches, place buns on waxed paper squares onto the steam plate, leaving 1 to 2 inches between buns.', 'Step 17': 'Cover and steam buns for 15 minutes.', 'Step 18': "Carefully remove the lid, so condensation doesn't drip onto buns.", 'Step 19': 'Continue steaming remaining buns until all are cooked.'
-        }, 
-        'tools': ['plate', 'bowl', 'wok'], 
-        'methods': 
-            ({'cook', 'bake', 'steam', 'heat', 'boil'}, {'leave', 'allow', 'punch', 'mix', 'reduce', 'work', 'keep', 'bring', 'set', 'sprinkle', 'spread', 'transfer', 'gather', 'triple', 'cover', 'let', 'wax', 'divide', 'roll', 'shape', 'face', 'double', 'drip', 'knead', 'continue', 'repeat', 'sit', 'remain', 'place', 'remove'})}
-
-
+    recipe = {'ingredients': [{'amount': '1 3/4', 'unit': 'cups', 'name': 'flour', 'descriptor': 'all-purpose', 'preparation': 'divided'}, {'amount': '3/4', 'unit': 'cup', 'name': 'water', 'descriptor': 'warm', 'preparation': 'divided'}, {'amount': '1', 'unit': 'tablespoon', 'name': 'active  yeast', 'descriptor': 'dry', 'preparation': ''}, {'amount': '1', 'unit': 'teaspoon', 'name': 'sugar', 'descriptor': 'white', 'preparation': ''}, {'amount': '2', 'unit': 'tablespoons', 'name': 'sugar', 'descriptor': 'white', 'preparation': ''}, {'amount': '1', 'unit': 'tablespoon', 'name': 'oil', 'descriptor': 'vegetable', 'preparation': ''}, {'amount': '1/4', 'unit': 'teaspoon', 'name': 'salt', 'descriptor': '', 'preparation': ''}, {'amount': '1/2', 'unit': 'teaspoon', 'name': 'baking powder', 'descriptor': '', 'preparation': ''}], 'directions': {'Step 1': 'Gather all ingredients.', 'Step 2': 'Mix together 1/4 cup flour, 1/4 cup warm water, yeast, and 1 teaspoon sugar in a large bowl.', 'Step 3': 'Allow to sit for 30 minutes.', 'Step 4': 'Mix in remaining 1 1/2 cups flour, remaining 1/2 cup warm water, 2 tablespoons sugar, vegetable oil, and salt.', 'Step 5': 'Knead until dough is smooth and elastic.', 'Step 6': 'Transfer to a greased bowl, roll to coat with oil, and let sit until tripled in size, 2 1/2 to 3 hours.', 'Step 7': 'Punch down dough and spread out on a floured board.', 'Step 8': 'Sprinkle baking powder evenly on surface of dough; knead for 5 minutes.', 'Step 9': 'Divide dough in half; set aside one half in a covered bowl.', 'Step 10': 'Divide remaining half into 12 equal pieces.', 'Step 11': 'Shape each into a ball; transfer each ball to a small square of waxed paper with the smooth surface facing up.', 'Step 12': 'Repeat portioning and shaping with remaining dough half.', 'Step 13': 'Cover all 24 dough balls and let sit until doubled in size, about 30 minutes.', 'Step 14': 'Bring some water to a boil in a wok, then reduce heat to medium and keep water at a low boil.', 'Step 15': 'Place the steam plate on a small wire rack in the middle of the wok, leaving at least 2 inches of space between the plate and the wok.', 'Step 16': 'Working in batches, place buns on waxed paper squares onto the steam plate, leaving 1 to 2 inches between buns.', 'Step 17': 'Cover and steam buns for 15 minutes.', 'Step 18': "Carefully remove the lid, so condensation doesn't drip onto buns.", 'Step 19': 'Continue steaming remaining buns until all are cooked.'}, 'tools': ['wok', 'plate', 'bowl'], 'methods': ({'bake', 'cook', 'boil', 'heat', 'steam'}, {'double', 'face', 'divide', 'transfer', 'remove', 'shape', 'place', 'keep', 'leave', 'punch', 'mix', 'reduce', 'sit', 'remain', 'knead', 'continue', 'drip', 'gather', 'spread', 'let', 'triple', 'cover', 'wax', 'roll', 'bring', 'allow', 'set', 'repeat', 'work', 'sprinkle'})}
     healthier_recipe = make_recipe_healthier(recipe)
     print(healthier_recipe)
